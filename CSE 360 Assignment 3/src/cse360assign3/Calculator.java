564f3107debacc43f6c9ccfef08484ccc16ab40a
Nicholas <nsstanto@asu.edu>
2016-02-21 18:53:30 -0700

    First commit of assignment 3.

---
+++ CSE 360 Assignment 3/src/cse360assign3/Calculator.java
@@ -0,0 +1,85 @@
+package cse360assign3;
+
+/** 
+ * Calculator is a class which generates an object to model a very simply calculator
+ * through a few methods. It holds a total value which can be accessed and allows 
+ * operations such as addition, subtraction, multiplication, division.
+ * @author Nicholas Stanton
+ * @version Feb 21, 2016 3:36PM
+ */
+
+public class Calculator 
+{
+	/** 
+	 * The integer value of the results of operations done by the calculator.
+	 */
+	private int total;
+	
+	/**
+	 * Initializes total to 0.
+	 */
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * A get method for the calculator's current total. 
+	 * @return total
+	 * The integer value of the results of operations done by calculator
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/**
+	 * A method for addition, adds a value to the total. 
+	 * @param value
+	 * the value to be added.
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**
+	 * A method for subtraction, subtracts a value from the total.
+	 * @param value
+	 * the value to be subtracted
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/**
+	 * A method for multiplication, multiplies the total by a value. 
+	 * @param value
+	 * the value for the multiplication
+	 */
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	/**
+	 * A method for division, divides the total by a value.
+	 * @param value
+	 * the value for the division
+	 */
+	public void divide (int value) 
+	{
+		
+	}
+	
+	/**
+	 * A method that shows the operations that calculator has done.
+	 * @return 
+	 * a string.
+	 */
+	public String getHistory () 
+	{
+		return "";
+	}
+}
